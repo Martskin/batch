a0b48929cd1c7b1d545835ff27f6012a7295c6e9
Martin Hofmann <martin@martinhofmann.com>
2015-08-15 15:45:39 -0400

    Initial commit.
    Adds first draft app files .

---
+++ assets/js/scripts.js
@@ -0,0 +1,41 @@
+/*!
+ * Batch
+ * A simple, responsive, flexbox grid system.
+ * https://github.com/Martskin/batch
+ * @author Martin Hofmann
+ * @version 1.0.0
+ * Copyright 2015. GPL-2.0 licensed.
+ */
+(function ($, window, document, undefined) {
+
+	'use strict';
+
+	$(function () {
+
+		setTimeout(function () {
+			$($('.bi-barrel').get().reverse()).each(function(i) {
+				var self = $(this);
+				setTimeout(function () {
+					$(self).addClass('is-dropped');
+				}, i*50);
+			});
+		}, 500);
+
+		$('[data-behavior="smooth-scroll"]').click(function(e){
+			e.preventDefault();
+			var target = $($(this).attr('href'));
+			var offset = target.offset();
+			$('html, body').animate({
+				scrollTop: offset.top
+			}, 500);
+		});
+
+		$('[data-behavior="trigger-click"]').click(function(e){
+			e.preventDefault();
+			var link = $(this).data("target");
+			$('[data-object="'+link+'"]')[0].click();
+		});
+
+	});
+
+})(jQuery, window, document);
